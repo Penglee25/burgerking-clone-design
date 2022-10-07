c6142c2f6a85ffc72aee1cc10028ac6a8bbea43b
Pee25 <65997785+Pee25@users.noreply.github.com>
2022-10-07 21:23:40 +0800

    change url

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<BrowserRouter>
 		<Routes>
-			<Route path="*" element={<App />} />
+			<Route path="https://burger-king-clone-design.netlify.app/" element={<App />} />
 		</Routes>
 	</BrowserRouter>
 );
